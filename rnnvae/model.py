639a9ce84b668669babf7b6ae7779f9ef21574aa
Gerard Martí <gerard.marti@upf.edu>
2020-09-03 11:47:47 +0200

    first skeleton

---
+++ rnnvae/model.py
@@ -0,0 +1,19 @@
+"""
+File containing the model of the network.
+"""
+
+class model_RNNVLayer():
+    """
+    Implementation of the specific layer described in the paper
+    """
+
+
+
+
+class model_RNNVAE():
+    """
+    This class implements the full model of the network.
+    We take as example the models of mcvae, to create it similar to them.
+    """
+
+
